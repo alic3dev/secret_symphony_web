d35ec1bc86849e79225f9208762e8900ee19a3cc
alic3 <alic3dev@gmail.com>
2024-11-28 16:40:31 -0500

    Make logo an anchor link

--- src/components/decorative/Logo.tsx
+++ src/components/decorative/Logo.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 import styles from '@/components/decorative/Logo.module.scss'
 
 export function Logo(): React.ReactElement {
   return (
-    <div className={styles.logo}>
+    <Link to="/" className={styles.logo}>
       <span>S</span>
       <div className={styles.bar} />
       <span>S</span>
-    </div>
+    </Link>
   )
 }
