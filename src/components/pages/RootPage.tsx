dba0b3e8f9445b9a2416a51516b883b0bbeb7254
alic3 <alic3dev@gmail.com>
2024-11-28 16:41:11 -0500

    Add temporary landing root page

--- src/components/pages/RootPage.tsx
+++ src/components/pages/RootPage.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router'
+
+import styles from '@/components/pages/RootPage.module.scss'
 
 export function RootPage(): React.ReactElement {
-  return <>Root Page</>
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.shadows} />
+        <div className={styles['shadows-secondary']} />
+
+        <h1 className={styles.title}>
+          <span>Secret</span> <span>Symphony</span>
+        </h1>
+
+        {/* <div className={styles.container}> */}
+        <p className={styles.description}>A secure communication platform.</p>
+
+        <Link to="/dashboard" className={styles.dashboard}>
+          Dashboard
+        </Link>
+        {/* </div> */}
+      </main>
+    </div>
+  )
 }
