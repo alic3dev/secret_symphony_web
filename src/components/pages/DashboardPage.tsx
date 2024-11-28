3142c3b05f56b458a5c42330384b2703e40ea7f0
alic3 <alic3dev@gmail.com>
2024-11-28 16:41:01 -0500

    Add dynamic data to dashboard

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -1,4 +1,10 @@
-import type { MessageData } from '@/components/thread/types'
+import type { UUID } from 'crypto'
+
+import type {
+  MessageData,
+  ReplyMessageData,
+  ThreadData,
+} from '@/components/thread/types'
 
 import React from 'react'
 
@@ -7,27 +13,52 @@ import { Thread } from '@/components/thread'
 
 import styles from '@/components/pages/DashboardPage.module.scss'
 
-const mockMessages: MessageData[] = [
+const mockThreads: ThreadData[] = [
   {
-    content: 'This is the starting message',
-    direction: 'to',
+    id: crypto.randomUUID(),
+    from: {
+      image: 'https://vite.dev/viteconf.svg',
+      name: 'Some Person',
+    },
+    messages: [
+      {
+        content: 'This is the starting message',
+        direction: 'to',
+      },
+      {
+        content: 'Well hello',
+        direction: 'from',
+      },
+      {
+        content: '...who is this?',
+        direction: 'from',
+      },
+      {
+        content: 'Alice lol',
+        direction: 'to',
+      },
+    ],
   },
   {
-    content: 'Well hello',
-    direction: 'from',
+    id: crypto.randomUUID(),
+    from: {
+      name: 'Another Person',
+    },
+    messages: [],
   },
   {
-    content: '...who is this?',
-    direction: 'from',
-  },
-  {
-    content: 'Alice lol',
-    direction: 'to',
+    id: crypto.randomUUID(),
+    from: {
+      name: 'The third Person',
+    },
+    messages: [],
   },
 ]
 
 import _wordList from '@/data/wordlists/english.json'
 const wordList: string[] = _wordList as string[]
+
+const DEV_SEND_RANDOM_MESSAGES: boolean = true
 
 function generateRandomMessage(): MessageData {
   let content: string = ''
@@ -39,36 +70,43 @@ function generateRandomMessage(): MessageData {
 
   return {
     content,
-    direction: Math.random() > 0.5 ? 'to' : 'from',
+    direction: Math.random() > 0.6 ? 'to' : 'from',
   }
 }
 
 export function DashboardPage(): React.ReactElement {
-  const [messages, setMessages] = React.useState<MessageData[]>([
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-    ...mockMessages,
-  ])
+  const [selectedThread, setSelectedThread] = React.useState<UUID>(
+    mockThreads[0].id,
+  )
+
+  const threadData = React.useMemo<ThreadData | undefined>(
+    (): ThreadData | undefined =>
+      mockThreads.find(
+        (thread: ThreadData): boolean => thread.id === selectedThread,
+      ),
+    [selectedThread],
+  )
+
+  const [newMessages, setNewMessages] = React.useState<MessageData[]>([])
+
+  const messages = React.useMemo<MessageData[]>(
+    (): MessageData[] => [...(threadData?.messages ?? []), ...newMessages],
+    [threadData, newMessages],
+  )
 
   React.useEffect((): (() => void) => {
     let timeoutId: number
 
     const sendRandomMessage = (): void => {
       timeoutId = window.setTimeout((): void => {
-        setMessages((prevMessages: MessageData[]): MessageData[] => {
-          return [...prevMessages, generateRandomMessage()]
-        })
+        if (DEV_SEND_RANDOM_MESSAGES) {
+          setNewMessages((prevNewMessages: MessageData[]): MessageData[] => {
+            return [...prevNewMessages, generateRandomMessage()]
+          })
+        }
 
         sendRandomMessage()
-      }, Math.random() * 500 + 100)
+      }, Math.random() * 10000 + 1000)
     }
 
     sendRandomMessage()
@@ -78,16 +116,36 @@ export function DashboardPage(): React.ReactElement {
     }
   }, [])
 
+  const selectThread = React.useCallback((newThreadId: UUID): void => {
+    setSelectedThread(newThreadId)
+    setNewMessages([])
+  }, [])
+
+  const sendMessage = React.useCallback(
+    (replyMessageData: ReplyMessageData): void => {
+      setNewMessages((prevNewMessages: MessageData[]): MessageData[] => [
+        ...prevNewMessages,
+        { direction: 'to', ...replyMessageData },
+      ])
+    },
+    [],
+  )
+
   return (
     <>
       <Header />
 
       <div className={styles.content}>
-        <Sidebar />
+        <Sidebar
+          threads={mockThreads}
+          selectThread={selectThread}
+          selectedThread={selectedThread}
+        />
 
         <Thread
-          images={{ from: 'https://vite.dev/viteconf.svg', to: '' }}
+          images={{ from: threadData?.from.image, to: '/thread-image.jpeg' }}
           messages={messages}
+          sendMessage={sendMessage}
         />
       </div>
     </>
