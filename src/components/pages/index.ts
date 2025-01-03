2e76825c06f3374b76527929bdc2347cd36153a1
alic3 <alic3dev@gmail.com>
2025-01-02 19:03:10 -0500

    Add `pages/index`

---
+++ src/components/pages/index.ts
@@ -0,0 +1,4 @@
+export { DashboardPage } from '@/components/pages/DashboardPage'
+export { LoginPage } from '@/components/pages/LoginPage'
+export { LogoutPage } from '@/components/pages/LogoutPage'
+export { RootPage } from '@/components/pages/RootPage'
