0b24e2dfc5e0c32b9ac8ae4d5383c7ee2ddd3875
alic3 <alic3dev@gmail.com>
2025-01-02 19:02:30 -0500

    Add `Login` button to header depending on if logged in or not

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,27 @@ import React from 'react'
 
 import { Logo } from '@/components/decorative/Logo'
 
+import { clearStoredId } from '@/utils/identity'
+
 import styles from '@/components/layout/Header.module.scss'
 
 export function Header(): React.ReactElement {
+  const logout = React.useCallback<() => void>((): void => {
+    clearStoredId()
+    window.location.href = '/'
+  }, [])
+
   return (
     <div className={styles.header}>
       <Logo />
+
+      {window.id ? (
+        <button onClick={logout}>X</button>
+      ) : (
+        <a className="button" href="/login">
+          &gt;
+        </a>
+      )}
     </div>
   )
 }
