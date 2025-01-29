34c7eb7e4bb3f1296736302b4c03238606bd137b
alic3 <alic3dev@gmail.com>
2025-01-29 15:47:44 -0500

    Add titles and links to `ImageBubble`s

--- src/components/ImageBubble.tsx
+++ src/components/ImageBubble.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 import styles from '@/components/ImageBubble.module.scss'
 
 export interface ImageBubbleProps {
   src?: string
+  href?: string
+  title?: string
+  large?: boolean
 }
 
-export function ImageBubble({ src }: ImageBubbleProps): React.ReactElement {
+export function ImageBubble(parameters: ImageBubbleProps): React.ReactElement {
+  const image: React.ReactElement = React.useMemo(() => {
+    return <img src={parameters.src} alt={parameters.title} />
+  }, [parameters.src, parameters.title])
+
+  if (parameters.href) {
+    return (
+      <Link
+        to={parameters.href}
+        className={`${styles['image-wrapper']}  ${
+          parameters.large ? styles.large : ''
+        }`}
+      >
+        {image}
+      </Link>
+    )
+  }
+
   return (
-    <div className={styles['image-wrapper']}>
-      <img src={src} />
+    <div
+      className={`${styles['image-wrapper']} ${
+        parameters.large ? styles.large : ''
+      }`}
+    >
+      {image}
     </div>
   )
 }
